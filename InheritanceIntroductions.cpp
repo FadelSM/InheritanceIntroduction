d326406a80c85e50fde8370d0b71e84558709a56
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:37:28 +0700

    Implement description method in Isosceles class
    Added description method to Isosceles class.

--- InheritanceIntroductions.cpp
+++ InheritanceIntroductions.cpp
@@ -15,10 +15,13 @@ class Triangle{
 
 class Isosceles : public Triangle{
     public:
-    	void isosceles(){
-    		cout<<"I am an isosceles triangle\n";
-    	}
-  		//Write your code here.
+        void isosceles(){
+            cout<<"I am an isosceles triangle\n";
+        }
+        
+        void description(){
+            cout<<"In an isosceles triangle two sides are equal\n";
+        }
 };
 
 int main(){
